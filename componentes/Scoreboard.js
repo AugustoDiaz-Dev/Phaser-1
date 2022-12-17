d03d7c51080ded062e8e35cf977939e2a5d29562
AugustoDiaz-Dev <augustodiaz_1@hotmail.com>
2022-12-17 19:44:30 +1000

    First version

---
+++ componentes/Scoreboard.js
@@ -0,0 +1,20 @@
+export class Scoreboard {
+    constructor(scene) {
+        this.relatedScene = scene;
+        this.score = 0;
+    }
+
+      create() {
+        this.scoreText = this.relatedScene.add.text(16,16, 'Score: 0', {
+            fontSize: '20px', 
+            // fill: '#fff', 
+            fontFamily: 'verdana, arial, sans-serif'
+          });
+      }
+
+      incrementPoints(points) {
+        this.score += points;
+        this.scoreText.setText('Score: ' + this.score);
+      }
+
+}
